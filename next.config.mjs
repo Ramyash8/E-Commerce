dca57931d6f3ca56dda089400db8a15361a0c223
Yash Ram <yashramt@gmail.com>
2025-08-10 18:08:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,56 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'cdn.fynd.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'cdn.thewirecutter.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.meesho.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'jivisa.in',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images-static.nykaa.com',
-        port: '',
-        pathname: '/**',
-      }
+      },
+      {
+        protocol: 'https' ,
+        hostname: 'placehold.co',
+      },
+       {
+        protocol: 'https',
+        hostname: 'www.beyours.in',
+      },
     ],
   },
 };
